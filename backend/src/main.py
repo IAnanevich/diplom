1d5746bab13aa95d474d3232d538f3608c25c653
Ivan Ananevich <ivanshahter71@gmail.com>
2022-02-20 21:00:05 +0300

    Add sinus calculation

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,25 @@
-import random
+import math
 
 from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
 
+def calculate_sin(x):
+    return math.sin(x)
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     print('Accepting client connection...')
+    x = 0
     await websocket.accept()
     while True:
         try:
-            # Wait for any message from the client
             await websocket.receive_text()
-            # Send message to the client
-            resp = {'value': random.uniform(0, 1)}
+            resp = {'x': x, 'y': calculate_sin(x)}
             await websocket.send_json(resp)
+            x += 1
         except Exception as e:
             print('error:', e)
             break
