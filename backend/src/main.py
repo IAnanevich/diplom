77fa3124942e776c8d6ba81afb305a9d3664bb61
Ivan Ananevich <ivanshahter71@gmail.com>
2022-03-28 14:12:44 +0300

    Add Binary

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,6 @@
 import math
 
-import numpy as np
 from fastapi import FastAPI, WebSocket
-
-from src.utils.calculation import MainCalculation
-from src.utils.constants import N
 
 app = FastAPI()
 
@@ -21,26 +17,10 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         try:
             resp = {'x': x, 'y': calculate_sin(x, data.get('a'), data.get('b'))}
-            await websocket.send_json(resp)
+            await websocket.send_json(data=resp, mode='binary')
             x += 1
+            if x == 50:
+                await websocket.close()
         except Exception as e:
             print('error:', e)
             break
-
-
-if __name__ == '__main__':
-    te1 = np.empty(N)
-    te2 = np.empty(N)
-    tl1 = np.empty(N)
-    tl2 = np.empty(N)
-    x = np.empty(N)
-
-    # TODO: change, these variables will come from client
-    fl = 10
-    t = 0
-    dt = 10
-
-    while True:
-        result = MainCalculation(te1=te1, te2=te2, tl1=tl1, tl2=tl2, x=x).calculation(dt=dt, fl=fl, t=t)
-        t += 1
-    print(result)
