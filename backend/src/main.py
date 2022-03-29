b462ed3353edb0582b2e42176539629a031b220c
Ivan Ananevich <ivanshahter71@gmail.com>
2022-03-29 17:37:41 +0300

    Add Sinus 2d

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,22 +5,32 @@ from fastapi import FastAPI, WebSocket
 app = FastAPI()
 
 
-def calculate_sin(x, a, b):
-    return math.sin(a * x + b)
+def calculate_sin(x, a, b, c, y, d):
+    return math.sin(a * x + b) + math.sin(c * y + d)
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    x = 0
+
     await websocket.accept()
     data = await websocket.receive_json()
-    while True:
-        try:
-            resp = {'x': x, 'y': calculate_sin(x, data.get('a'), data.get('b'))}
-            await websocket.send_json(data=resp, mode='binary')
-            x += 1
-            if x == 50:
-                await websocket.close()
-        except Exception as e:
-            print('error:', e)
-            break
+    try:
+        a = data.get('a')
+        b = data.get('b')
+        c = data.get('c')
+        d = data.get('d')
+        lim_x = data.get('lim_x')
+        lim_y = data.get('lim_y')
+
+        for x in range(lim_x):
+            result_y = []
+            result_sin = []
+            for y in range(lim_y):
+                result_sin.append(calculate_sin(x, a, b, c, y, d))
+                result_y.append(y)
+            resp = {'x': x, 'y': result_y, 'z': result_sin}
+            await websocket.send_json(data=resp, mode='text')
+
+        await websocket.close()
+    except Exception as e:
+        print('error:', e)
