ebf002619198349d5864b5479c14bd97c3ad1c9c
Dmitrij Podabed <dmitrij.podabed@anais.digital>
2022-04-11 01:07:38 +0300

    TESTING COMMIT TO CHECK STEPS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,11 +9,15 @@ function App() {
   const [inputValueA, setInputValueA] = useState('');
   const [inputValueC, setInputValueC] = useState('');
   const [inputValueD, setInputValueD] = useState('');
-  const [pointsXArray, setPointsXArray] = useState<number[][]>([]);
-  const [pointsYArray, setPointsYArray] = useState<number[]>([]);
-  const [pointsZArray, setPointsZArray] = useState<number[]>([]);
+  const [pointsXArray, setPointsXArray] = useState<any>([]);
+  const [pointsYArray, setPointsYArray] = useState<any>([]);
+  const [pointsZArray, setPointsZArray] = useState<any>([]);
   const [pointsXArray3d, setPointsXArray3d] = useState<number[][]>([]);
   const [pointsYArray3d, setPointsYArray3d] = useState<number[][]>([]);
+
+  const [pointsXArrayTmp, setPointsXArrayTmp] = useState<any>([]);
+  const [pointsYArrayTmp, setPointsYArrayTmp] = useState<any>([]);
+  const [pointsZArrayTmp, setPointsZArrayTmp] = useState<any>([]);
 
   const [counter, setCounter] = useState<number>(0);
 
@@ -48,8 +52,12 @@ function App() {
           return [...prevState, array.fill(dataObj.x)];
         });
 
-        setPointsYArray((prevState) => {
+        /*        setPointsYArray((prevState) => {
           return [...prevState, dataObj.y[dataObj.x]];
+        });*/
+
+        setPointsYArray((prevState) => {
+          return [...prevState, dataObj.y];
         });
 
         setPointsYArray3d((prevState) => {
@@ -58,10 +66,6 @@ function App() {
 
         setPointsZArray((prevState) => {
           return [...prevState, dataObj.z];
-        });
-
-        setCounter((prevCounter) => {
-          return prevCounter + 1;
         });
       });
     };
@@ -77,22 +81,21 @@ function App() {
       <Plot
         data={[
           {
-            x: pointsXArray,
-            y: pointsYArray,
-            z: pointsZArray,
-            type: 'contour',
-          },
-        ]}
-        layout={{ width: 800, height: 500, title: 'Sinus Plot' }}
-      />
-
-      <Plot
-        data={[
-          {
             x: pointsXArray3d,
             y: pointsYArray3d,
             z: pointsZArray,
             type: 'surface',
+          },
+        ]}
+        layout={{ width: 800, height: 500, title: 'Sinus Plot' }}
+      />
+      <Plot
+        data={[
+          {
+            x: pointsXArrayTmp,
+            y: pointsYArrayTmp,
+            z: pointsZArrayTmp,
+            type: 'contour',
           },
         ]}
         layout={{ width: 800, height: 500, title: 'Sinus Plot' }}
@@ -150,13 +153,49 @@ function App() {
         </button>
         <button
           onClick={() => {
-            console.log('x: ', pointsXArray);
-            console.log('y: ', pointsYArray);
-            console.log('z: ', pointsZArray);
+            console.log('pointsXArray: ', pointsXArray);
+            console.log('pointsYArray: ', pointsYArray);
+            console.log('pointsZArray: ', pointsZArray);
+
+            console.log('pointsXArrayTmp: ', pointsXArrayTmp);
+            console.log('pointsYArrayTmp: ', pointsYArrayTmp);
+            console.log('pointsZArrayTmp: ', pointsZArrayTmp);
             console.log('counter: ', counter);
           }}
         >
           {'log states'}
+        </button>
+        <button
+          onClick={() => {
+            setPointsXArrayTmp((prev) => {
+              console.log('pointsXArray[counter]: ', pointsXArray[counter]);
+
+              if (counter !== 0) {
+                return [...prev, pointsXArray[counter]];
+              }
+              return [pointsXArray[counter]];
+            });
+
+            setPointsYArrayTmp((prev) => {
+              if (counter !== 0) {
+                return [...prev, pointsYArray[counter]];
+              }
+              return [pointsYArray[counter]];
+            });
+
+            setPointsZArrayTmp((prev) => {
+              if (counter !== 0) {
+                return [...prev, pointsZArray[counter]];
+              }
+              return [pointsZArray[counter]];
+            });
+
+            setCounter((prevCounter) => {
+              return prevCounter + 1;
+            });
+          }}
+        >
+          {'step'}
         </button>
       </div>
     </div>
