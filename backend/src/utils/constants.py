1abca9bfd0b14c48ce2da88460636ffaf6dfcc86
Alexey Kurilo <akurilo@motivemetrics.com>
2022-05-29 23:06:49 +0300

    hz che tam

--- backend/src/utils/constants.py
+++ backend/src/utils/constants.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 nx = 50
 ny = 50
 nz = 50
@@ -13,19 +12,12 @@ dz = 0.2
 tt = 0.0
 ntime = 0.0
 
-x = np.linspace(0, (nx+1) * dx, nx+1)
-y = np.linspace(0, (ny+1) * dy, ny+1)
-z = np.linspace(0, (nz+1) * dz, nz+1)
+x = np.linspace(0, (nx + 1) * dx, nx + 1)
+y = np.linspace(0, (ny + 1) * dy, ny + 1)
+z = np.linspace(0, (nz + 1) * dz, nz + 1)
 
 # variable for initial conditions
 init_var = 1e-16
-
-# TODO: from frontend
-r0 = 1e-2  # - radius of light beam, cm
-kabs = 5e5  # - ansorption coeff, cm-1
-P0 = 1e8  # - intensity, W/cm2
-tp = 1e-13  # - pulse duration, s
-# ------------------------------------------
 
 # heat cond electron, W/cmK
 kte = 3.115
@@ -53,33 +45,21 @@ gamma = 0.25e+11
 
 init_val = 1e-16
 
-F     = np.full((nx+1, ny+1, nz+1), init_val)
-tmpe0 = np.full((nx+1, ny+1, nz+1), init_val)
-tmpe1 = np.full((nx+1, ny+1, nz+1), init_val)
-tmpe2 = np.full((nx+1, ny+1, nz+1), init_val)
+F = np.full((nx + 1, ny + 1, nz + 1), init_val)
+tmpe0 = np.full((nx + 1, ny + 1, nz + 1), init_val)
+tmpe1 = np.full((nx + 1, ny + 1, nz + 1), init_val)
+tmpe2 = np.full((nx + 1, ny + 1, nz + 1), init_val)
 
-tmpi0 = np.full((nx+1, ny+1, nz+1), init_val)
-tmpi1 = np.full((nx+1, ny+1, nz+1), init_val)
-tmpi2 = np.full((nx+1, ny+1, nz+1), init_val)
+tmpi0 = np.full((nx + 1, ny + 1, nz + 1), init_val)
+tmpi1 = np.full((nx + 1, ny + 1, nz + 1), init_val)
+tmpi2 = np.full((nx + 1, ny + 1, nz + 1), init_val)
 
-xe = np.exp(-(x[(nx+1)//2] - x) ** 2)
-ye = np.exp(-(y[(ny+1)//2] - y) ** 2)
+xe = np.exp(-(x[(nx + 1) // 2] - x) ** 2)
+ye = np.exp(-(y[(ny + 1) // 2] - y) ** 2)
 ze = np.exp(-z)
 
-fe = np.repeat(xe, ny+1).reshape((nx+1, ny+1))
+fe = np.repeat(xe, ny + 1).reshape((nx + 1, ny + 1))
 fe[:, :] *= ye
 
-fe = np.repeat(fe, nz+1).reshape((nx+1, ny+1, nz+1))
+fe = np.repeat(fe, nz + 1).reshape((nx + 1, ny + 1, nz + 1))
 fe *= ze
-
-# char time, s
-t0 = 1 / (kabs * u0)
-
-# parameter of pulse
-beta = t0 / tp
-
-a1 = kte * t0 / (roe * Ce * r0**2)
-a2 = kabs**2 * r0**2
-b1 = kabs * P0 * t0 * beta / (roe * Ce * T00)
-cc1 = gamma * t0 / (roe * Ce)
-cc2 = gamma * t0 / (roi * Ci)
