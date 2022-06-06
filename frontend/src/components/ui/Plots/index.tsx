5782d7c6c0972cd006b0cb956be171f2f7ea03d3
Dmitrij Podabed <dmitrij.podabed@anais.digital>
2022-06-06 22:40:48 +0300

    add new plots

--- frontend/src/components/ui/Plots/index.tsx
+++ frontend/src/components/ui/Plots/index.tsx
@@ -3,32 +3,83 @@ import { FC } from 'react';
 import { PlotsPropsType } from './types';
 
 export const Plots: FC<PlotsPropsType> = (props) => {
-  const { pointsXArray, pointsYArray, pointsTempEArray, pointsTempIArray } = props;
+  const {
+    pointsY1TimeArray,
+    pointsY2TimeArray,
+    pointsTempEArray,
+    pointsTempIArray,
+    pointsYe2Array,
+    pointsYi2Array,
+    pointsZArray,
+    time,
+  } = props;
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <Plot
-        data={[
-          {
-            x: pointsXArray,
-            y: pointsYArray,
-            z: pointsTempEArray,
-            type: 'contour',
-          },
-        ]}
-        layout={{ width: 500, height: 400, title: 'Температура электронного газа' }}
-      />
-      <Plot
-        data={[
-          {
-            x: pointsXArray,
-            y: pointsYArray,
-            z: pointsTempIArray,
-            type: 'contour',
-          },
-        ]}
-        layout={{ width: 500, height: 400, title: 'Температура ионной решётки' }}
-      />
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Plot
+          data={[
+            {
+              x: pointsY1TimeArray,
+              y: pointsY2TimeArray,
+              z: pointsTempEArray,
+              type: 'contour',
+            },
+          ]}
+          layout={{ width: 500, height: 400, title: 'Температура электронного газа' }}
+        />
+        <Plot
+          data={[
+            {
+              x: pointsY1TimeArray,
+              y: pointsY2TimeArray,
+              z: pointsTempIArray,
+              type: 'contour',
+            },
+          ]}
+          layout={{ width: 500, height: 400, title: 'Температура ионной решётки' }}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Plot
+          data={[
+            {
+              x: pointsZArray,
+              y: pointsYe2Array,
+              type: 'scatter',
+            },
+            {
+              x: pointsZArray,
+              y: pointsYi2Array,
+              type: 'scatter',
+            },
+          ]}
+          layout={{
+            width: 500,
+            height: 400,
+            title: 'Te, Ti',
+          }}
+        />
+        <Plot
+          data={[
+            {
+              x: time,
+              y: pointsY1TimeArray,
+              type: 'scatter',
+            },
+            {
+              x: time,
+              y: pointsY2TimeArray,
+              type: 'scatter',
+            },
+          ]}
+          layout={{
+            width: 500,
+            height: 400,
+            title: 'Te, Ti',
+          }}
+        />
+      </div>
     </div>
   );
 };
